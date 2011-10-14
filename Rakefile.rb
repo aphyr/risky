b606b4bc56f217909dabe4dfac16e8c19fd030bf
Aphyr <aphyr@aphyr.com>
2011-10-14 13:14:24 -0700

    Version 0.1.7. Only change is riak-client dep 1.0.0.beta

--- Rakefile.rb
+++ Rakefile.rb
@@ -29,7 +29,7 @@ gemspec = Gem::Specification.new do |s|
  
   s.required_ruby_version = '>= 1.8.6'
  
-  s.add_dependency('riak-client', '~> 1.0.0.pre')
+  s.add_dependency('riak-client', '~> 1.0.0.beta')
 end
  
 Rake::GemPackageTask.new(gemspec) do |p|
